9901cec8b5023af699056cf3f9c69c1507476b65
Temporary Username <temp@Temporarys-MacBook-Pro.local>
2021-01-16 22:14:42 +0200

    add react-router-dom add basic routes add basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        app good
-      </header>
+      <BrowserRouter>
+        <Header />
+        <AppRouter />
+      </BrowserRouter>
     </div>
   );
 }
